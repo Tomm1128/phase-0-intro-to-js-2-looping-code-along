1e93a6714ee654b8a55fa4299ea7a102b3b577e0
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-08 10:06:24 -0400

    Add function that makes card messages

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+const writeCards = (names, event) => {
+  const cards = []
+  for (let i = 0; i < names.length; i++ ){
+      cards[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+  }
+
+  return cards
+}
+
